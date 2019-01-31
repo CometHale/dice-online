38b575d400164571338fc873bd1275adf8627a25
Haley Sanders <haleyfsanders@gmail.com>
2019-01-31 09:31:52 -0500

    Adding Dice Controller unit tests and stubs for others

---
+++ app/api/controller/usercontroller_test.go
@@ -0,0 +1,19 @@
+package controller_test
+
+import "testing"
+
+func TestUserGet(t *testing.T) {
+
+}
+
+func TestUserCreate(t *testing.T) {
+
+}
+
+func TestUserLogin(t *testing.T) {
+
+}
+
+func TestUserLogout(t *testing.T) {
+
+}
