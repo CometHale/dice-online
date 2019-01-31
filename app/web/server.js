08a3be5faeb996b246333bca75099df6ddeb169f
Haley Sanders <haleyfsanders@gmail.com>
2019-01-31 13:34:01 -0500

    update start in package.json

--- app/web/server.js
+++ app/web/server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const serveStatic = require("serve-static")
 const path = require('path');
-app = express();
+const app = express();
+const port = process.env.PORT || 8080;
+
 app.use(serveStatic(path.join(__dirname, 'dist')));
-const port = process.env.PORT || 80;
 app.listen(port);
